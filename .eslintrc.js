89faaa36c32663aa3b0a5f441ed6511ec43ff919
lijie5690 <290624043@qq.com>
2026-05-11 14:29:33 +0300

    sne

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,15 @@ module.exports = {
     node: true,
   },
   extends: [
-    'airbnb-base',
+    'airbnb-base', // 使用 Airbnb 风格
   ],
   parserOptions: {
-    ecmaVersion: 11,
+    ecmaVersion: 2018, // 注意这里是 2018
   },
   rules: {
+    // 这里是自定义规则区域
+    // 例如：强制使用分号，禁止使用 console
+    'semi': ['error', 'always'],
     'no-console': 'off',
-  },
+  }
 };
